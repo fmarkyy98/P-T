80b5aa531f062630af91848e2b36ce57ab28a519
Fekete Márk <fmark98@gmail.com>
2019-08-09 14:39:48 +0200

    Ready for first tests.

--- Practise Tool/Practise Tool/Program.cs
+++ Practise Tool/Practise Tool/Program.cs
@@ -6,7 +6,53 @@ namespace Practise_Tool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 0, y = 0, Solution = 0;
+            char operation;
+            Console.WriteLine("Write down the two operator splitted with ';' character!");
+            string input = Console.ReadLine();
+            bool failure = false;
+            do
+            {
+                if (failure)
+                {
+                    Console.WriteLine("Incorrect sintax, write again the input!");
+                    input = Console.ReadLine();
+                }
+                string[] split = input.Split(';');
+                if (!(int.TryParse(split[0], out x) && int.TryParse(split[1], out y))) failure = true;
+            } while (failure);
+            Console.WriteLine("write down the operator!");
+            input = Console.ReadLine();
+            failure = false;
+            do
+            {
+                if (failure)
+                {
+                    Console.WriteLine("Incorrect sintax, write again the input!");
+                    input = Console.ReadLine();
+                }
+                if (!char.TryParse(input, out operation)) { failure = true; break; }
+                switch (operation)
+                {
+                    case '+':
+                        Solution = x + y;
+                        break;
+                    case '-':
+                        Solution = x - y;
+                        break;
+                    case '*':
+                        Solution = x * y;
+                        break;
+                    case '/':
+                        Solution = x / y;
+                        break;
+                    default:
+                        failure = true;
+                        break;
+                }
+            } while (failure);
+            Console.WriteLine($"{x} {operation} {y} = {Solution}");
+            Console.ReadKey();
         }
     }
 }
